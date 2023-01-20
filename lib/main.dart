0927a2890497a48c4ef63d3ebc7eceea7c782cce
IanTaiAhn <iantaiahn@yahoo.com>
2023-01-19 20:03:09 -0700

    I like this layout much better.

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// Literally everything is a damn widget.
+// Widgetception
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // We are building the main app here, which is a stateless widget.
+    // We have to return something that gives structure, and MaterialApp is
+    // quite usefull since it has a bunch of other widgets that work with it.
+    // We are returning a StatefulWidget that returns Scaffold.
+    return const MaterialApp(home: MyStatefulWidget());
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static final List<Widget> _widgetOptions = <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        const Text(
+          'Index 0: Home',
+          style: optionStyle,
+        ),
+        const Text("more text on home stuff")
+      ],
+    ),
+    const Text(
+      'Index 1: Contacts',
+      style: optionStyle,
+    ),
+    const Text(
+      'Index 2: Messages',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+        backgroundColor: const Color.fromARGB(255, 232, 157, 182),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.perm_contact_cal_rounded),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message_rounded),
+            label: 'Messages',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
